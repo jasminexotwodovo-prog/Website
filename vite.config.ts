cca86dfc382ee74ee2bb9b1582ce12de91155e80
jasminexotwodovo-prog <jasminexotwodovo@gmail.com>
2025-09-02 19:55:16 -0700

    Update vite.config.ts
    Delete

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: "/silkveil-pastel/",
-  plugins: [react()],
-  server: { port: 5173 }
-})
